09bc8ec88322a3eebb9085092ad8aea05a0ab922
bshevlin <bshevlin@knights.ucf.edu>
2015-05-29 16:12:44 -0400

    added basic functionality: prints a list of mounts

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-//Barry Shevlin
-//
 var http = require("http");
 
+//calls back with an array of mount json objects from region/server/character combination
+var getCharacterMounts = function(region, server, character, callback){
+	http.get("http://"+region+".battle.net/api/wow/character/"+server+"/"+character+"?fields=mounts", function(stream){
+		stream.setEncoding("utf8");
+		stream.on("data", function(data){
+			var charObj = JSON.parse(data);
+			var mountArray = charObj.mounts.collected;
+			callback(mountArray);
+		})
+	});
+};
 
-var server = "shuhalo";	//these will obviously be changable later
-var character = "cowcao";
-var battleNetUrl = "http://us.battle.net/api/wow/character/"
-var fullUrl = battleNetUrl + server + "/" + character;
 
-var getCharacterMounts = function(server, character){//returns an array of mount json objects
-	
+
+var main = function(){//main method for testing
+	var rg = "us";
+	var srv = "shuhalo";	//these will obviously be changable later
+	var character = "kaocow";
+
+	//prints a list of mounts for a given character, etc.
+	getCharacterMounts(rg, srv, character, function(data){
+		console.log("You have "+data.length+" mounts:");
+		for(i=0;i<data.length;i++){
+			console.log(data[i].name);
+		}
+	});
 }
+main();
