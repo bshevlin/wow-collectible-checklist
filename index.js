f76f7ce921221da6efdafeb8e950be2b652330aa
bshevlin <bshevlin@knights.ucf.edu>
2015-06-02 13:43:25 -0400

    added achievements and stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var Q = require("q");
 //calls back with an array of mount json objects from region/server/character combination
 var getCharacterMounts = function(region, server, character, callback){
 	var http = require("http");
@@ -17,19 +18,154 @@ var getCharacterMounts = function(region, server, character, callback){
 	});
 };
 
+//same as above, but for companions. can include duplicates.
+var getCharacterPets = function(region, server, character, callback){
+	var http = require("http");
+	//var deffered = Q.defer();
+	http.get("http://"+region+".battle.net/api/wow/character/"+server+"/"+character+"?fields=pets", function(stream){
+		stream.setEncoding("utf8");
+
+		var data = "";
+		stream.on("data", function(d){
+			data += d;
+		});
+
+		stream.on("end", function(){
+			var charObj = JSON.parse(data);
+			var petArray = charObj.pets.collected;
+			callback(petArray);
+			//deferred.resolve(petArray);
+		});
+	});
+}
+
+//similar to above, but with achievements. Achievements have a much more complicated data structure.
+//gives an achievements object, not an array.
+var getCharacterAchievements = function(region, server, character, callback){
+		var http = require("http");
+		http.get("http://"+region+".battle.net/api/wow/character/"+server+"/"+character+"?fields=achievements", function(stream){
+		stream.setEncoding("utf8");
+
+		var data = "";
+		stream.on("data", function(d){
+			data += d;
+		});
+
+		stream.on("end", function(){
+			var charObj = JSON.parse(data);
+			var achieve = charObj.achievements;
+			callback(achieve);
+		});
+	});
+};
+
+//gets an array of achievement objects. one for each major category.
+var getAchievementData = function(region, callback){
+	var http = require("http");
+	http.get("http://"+region+".battle.net/api/wow/data/character/achievements", function(stream){
+		stream.setEncoding("utf8");
+
+		var data = "";
+		stream.on("data", function(d){
+			data += d;
+		});
 
 
-var main = function(){//main method for testing
+		stream.on("end", function(){
+			var achObj = JSON.parse(data);
+			var achieve = achObj.achievements;
+			callback(achieve);
+		});
+	});
+};
+
+//checks if an achievement has been earned. takes individual achievement object and character's record
+//calls back with true or false
+var checkAchievement = function(achievement, charAchieves, callback){
+	var id = achievement.id;
+	if(getIndex(id, charAchieves.achievementsCompleted) >= 0){
+		callback(true);
+	} else{
+		callback(false);
+	}
+}
+
+
+
+//binary search of a sorted array for a particular number
+var getIndex = function(num, array){
+	var length = array.length;
+	var low = 0;
+	var high = length-1;
+	var i;
+
+	while(low <= high){
+		i = ((high + low) / 2) | 0;
+		if(array[i] === num){
+			return i;
+		} else if(array[i] > num){
+			high = i-1;
+		} else{
+			low = i+1;
+		}
+
+	}
+	return -1;
+}
+
+
+//main method for testing
+var main = function(){
 	var rg = "us";
 	var srv = "shuhalo";	//these will obviously be changable later
-	var character = "kaocow";
+	var character = "rocktarded";
+	var characterAchievements;
+	var achievementData;
+	var steps = 0; //temp variable to make sure both callbacks have been done
 
 	//prints a list of mounts for a given character, etc.
 	getCharacterMounts(rg, srv, character, function(data){
+		var i;
 		console.log("You have "+data.length+" mounts:");
 		for(i=0;i<data.length;i++){
 			console.log(data[i].name);
 		}
+		console.log("");
 	});
+
+	getCharacterPets(rg, srv, character, function(data){
+		var i;
+		console.log("You have "+data.length+" pets:");
+		for(i=0;i<data.length;i++){
+			console.log(data[i].name);
+		}
+		console.log("");
+	});
+
+	getCharacterAchievements(rg, srv, character, function(data){
+		var list = data.achievementsCompleted;
+		characterAchievements = data;
+		console.log("You have "+list.length+" achievements. They will not be listed right now, but trust me.\n");
+		steps++;
+		if(steps===2)
+			main2();
+	});
+
+	getAchievementData(rg, function(data){
+		achievementData = data;
+		console.log("There are " +data.length + " categories of achievments.\n");
+		steps++;
+		if(steps ===2)
+			main2();
+	});
+
+	//testing checkAchievement
+	var main2 = function(){
+		console.log("\n\n____________________________________________\n\n");
+		checkAchievement(achievementData[0].achievements[8], characterAchievements, console.log);
+		checkAchievement(achievementData[0].achievements[9], characterAchievements, console.log);
+		checkAchievement(achievementData[0].achievements[10], characterAchievements, console.log);
+	}
 }
+
 main();
